58d6a38e19253fc628917377c35b961e7928f65c
Luz Ramirez <lramirezd2302@alumno.ipn.mx>
2026-01-20 09:00:08 -0600

    Se corrigio el minimo de digitos requeridos para iniciar sesión, paso de 6 a 3 para coincidir con lo pedido el en registro de usuario

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,7 +6,7 @@ dotenv.config();
 const pool = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'mono123.',
+    password: process.env.DB_PASSWORD || 'kasanajisa1.',
     database: process.env.DB_NAME || 'tooeasy',
     waitForConnections: true,
     connectionLimit: 10,
